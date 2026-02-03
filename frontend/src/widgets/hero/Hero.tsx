9ae9ed2772a64854abf0f573116e9cc8ed20436c
Mikhail Volodko <clavor.32@gmail.com>
2026-02-03 23:58:36 +0300

    style: little changes

--- frontend/src/widgets/hero/Hero.tsx
+++ frontend/src/widgets/hero/Hero.tsx
@@ -2,7 +2,7 @@ import { Search } from "@/components/search/Search.tsx";
 
 export const Hero = () => {
   return (
-    <div className='flex flex-col mt-10 gap-4'>
+    <div className='flex items-center flex-col mt-10 gap-4'>
       <h1 className='text-center w-full text-4xl'>How's the sky looking today</h1>
       <Search />
     </div>

e54a5d71874e0736106475171cea62819eee8235
Mikhail Volodko <clavor.32@gmail.com>
2026-02-04 22:33:16 +0300

    init: init right sidebar

---
+++ frontend/src/components/hourly-weather/HourlyWeather.tsx
@@ -0,0 +1,10 @@
+type Props = {
+
+}
+export const HourlyWeather = (props: Props) => {
+  return (
+    <section className='col-start-4 col-end-5 row-start-1 row-end-4 h-full'>
+      Hourly
+    </section>
+  )
+}
